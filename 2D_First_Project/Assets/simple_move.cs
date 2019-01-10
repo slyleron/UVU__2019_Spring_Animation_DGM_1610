8610381a0d40849e8dd7d824eab4e6cfbe04102c
skyler <skyleryoung1111@gmail.com>
2019-01-10 11:00:03 -0700

    Added simple movement, started collisions

--- 2D_First_Project/Assets/simple_move.cs
+++ 2D_First_Project/Assets/simple_move.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class simple_move : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float jupmhight=10;
+    float up = 0;
+    float right = 0;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //up down left right
+        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        right += h;
+        //comment this line out to have it not move up and down
+        up += v;
+        transform.position = new Vector3(right, up, 0);
+       // if(Collision2D.)
+       // { Debug.Log("hit"); }
+
+        
+        //Jump code
+        ///if (up < 5) { up += v; }
+        //else
+       //{
+        //    up += -0.01;
+       //}
+    }
+
+   
+
 }
